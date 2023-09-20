6570df777025b953e51b503552cdc7d9c238fb23
luyao699 <lucky99475@gmail.com>
2023-09-20 13:58:54 +0800

    ”cs“

---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Center, Flex } from "@chakra-ui/react";
+
+export const About: React.FC = () => {
+  return (
+    <Flex>
+      <Center>测试文件</Center>
+    </Flex>
+  );
+};
